ac1a95a5296ac32f23aab453b3a6eb0f4f655445
Eric Martinez <eric.michael.mtz@gmail.com>
2020-02-26 11:08:33 -0600

    add assignment

---
+++ static/critters.js
@@ -0,0 +1,5 @@
+
+var imgs = { 0 : "http://animalcontrolphx.com/wp-content/uploads/2013/05/gophers-400.jpg",
+             1 : "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/Q-Z/wolverine-crouching.adapt.945.1.jpg",
+             2 : "https://www.waikikiaquarium.org/wp-content/uploads/2013/11/octopus_620.jpg"
+           }
